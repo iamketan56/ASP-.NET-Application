0218e70cad452c1f53b418cf04aec1bf87f4efb6
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-09-01 11:52:31 +0530

    APP

---
+++ JokeApp/Models/Joke.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokeApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+
+        public string JokeQues { get; set; }
+
+        public string JokeAns { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
